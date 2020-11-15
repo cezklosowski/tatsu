96a9987cd2d76185a446db787b90be153395790f
cezklosowski <cez.klosowski@gmail.com>
2020-11-15 11:59:44 +0100

    PersonDtoTest

--- src/test/java/com/cezklosowski/module/person/PersonDtoTest.java
+++ src/test/java/com/cezklosowski/module/person/PersonDtoTest.java
@@ -14,13 +14,25 @@ public class PersonDtoTest {
 	public void shouldReturnEmptyObject() {
 		// given
 		// when
+			// przygotowanie danych
+				PersonDto personDto = new PersonDto();
 		// then
+			// asercje
+				assertThat(personDto.getName()).isNull();
+				assertThat(personDto.getSurname()).isNull();
+				assertThat(personDto.getEmail()).isNull();
+				assertThat(personDto.isEmailValid()).isFalse();
 	}
 
 	@Test
 	public void shouldReturnPerson() {
 		// given
 		// when
+		PersonDto personDto = new PersonDto(NAME,SURNAME,EMAIL);
 		// then
+		assertThat(personDto.getName()).isEqualTo("Michał");
+		assertThat(personDto.getSurname()).isEqualTo("Szałkowski");
+		assertThat(personDto.getEmail()).isEqualTo("no@michalszalkowski.com");
+		assertThat(personDto.isEmailValid()).isTrue();
 	}
 }

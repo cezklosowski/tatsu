2b555bcb5e0265a24a8c30e8a68487f57d5e4712
cezklosowski <cez.klosowski@gmail.com>
2020-11-15 14:15:31 +0100

    BookServiceTest

--- src/test/java/com/cezklosowski/module/books/dto/BookTypesTest.java
+++ src/test/java/com/cezklosowski/module/books/dto/BookTypesTest.java
@@ -2,22 +2,56 @@ package com.cezklosowski.module.books.dto;
 
 import org.junit.Test;
 
+import java.awt.print.Book;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class BookTypesTest {
 
 	@Test
 	public void shouldReturnDefaultEnumInCaseOfNullOrInvalidValue() {
-		assertThat(true).isTrue();
-		assertThat("a").isEqualTo("a");
+
+		// given
+		String typeNull = null;
+		String typeInvalid = "poetry";
+		String typeEmpty = "";
+
+		// when
+		BookTypes bookTypeNull = BookTypes.byString(typeNull);
+		BookTypes bookTypeInvalid = BookTypes.byString(typeInvalid);
+		BookTypes bookTypeEmpty = BookTypes.byString(typeEmpty);
+
+		// then
+
+		assertThat(bookTypeNull).isEqualTo(BookTypes.CLASSIC);
+		assertThat(bookTypeInvalid).isEqualTo(BookTypes.CLASSIC);
+		assertThat(bookTypeEmpty).isEqualTo(BookTypes.CLASSIC);
 	}
 
 	@Test
 	public void shouldReturnEnumByString() {
+
+		// given
+		String typeString = "ACTION";
+
+		// when
+		BookTypes bookType = BookTypes.byString(typeString);
+
+		// then
+		assertThat(bookType).isEqualTo(BookTypes.ACTION);
 	}
 
 	@Test
 	public void shouldReturnEnumByStringAndIgnoreCase() {
+
+		// given
+		String typeString = "actiON";
+
+		// when
+		BookTypes bookType = BookTypes.byString(typeString);
+
+		// then
+		assertThat(bookType).isEqualTo(BookTypes.ACTION);
 	}
 
 }
--- src/test/java/com/cezklosowski/module/books/service/BookServiceTest.java
+++ src/test/java/com/cezklosowski/module/books/service/BookServiceTest.java
@@ -6,7 +6,11 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
+
+
+import static org.mockito.Mockito.times;
 
 @RunWith(MockitoJUnitRunner.class)
 public class BookServiceTest {
@@ -23,43 +27,72 @@ public class BookServiceTest {
 
 	@Test
 	public void shouldCallGetOneWhenIdIsValid() {
+
 		// when
+		bookService.getOne(VALID_BOOK_ID);
+
 		// then
+		Mockito.verify(bookRepository).getOne(VALID_BOOK_ID);
 	}
 
 	@Test
 	public void shouldNotCallGetOneWhenIdIsValid() {
+
 		// when
+		bookService.getOne(NOT_VALID_BOOK_ID);
+
 		// then
+		Mockito.verify(bookRepository, times(0)).getOne(NOT_VALID_BOOK_ID);
 	}
 
 	@Test
 	public void shouldCallFindAll() {
+
 		// when
+		bookService.getAll();
+
 		// then
+		Mockito.verify(bookRepository).findAll();
 	}
 
 	@Test
-	public void shouldCallSaveAndFlash() {
+	public void shouldCallDaoSaveAndFlashDuringCreation() {
+
 		// when
+		bookService.create(BOOK);
+
 		// then
+		Mockito.verify(bookRepository).saveAndFlush(BOOK);
 	}
 
 	@Test
-	public void shouldNotCallSaveAndFlashWhenObjectIsEqualToNull() {
+	public void shouldNotCallDaoSaveAndFlashWhenObjectIsEqualToNull() {
+
 		// when
+		bookService.create(null);
+
 		// then
+		Mockito.verify(bookRepository, times(0)).saveAndFlush(null);
 	}
 
 	@Test
-	public void shouldCallDeleteById() {
+	public void shouldCallDaoDeleteById() {
+
 		// when
+		bookService.delete(VALID_BOOK_ID);
+
 		// then
+		Mockito.verify(bookRepository).deleteById(VALID_BOOK_ID);
+
 	}
 
 	@Test
 	public void shouldSaveAndFlashInCaseOfUpdate() {
+
 		// when
+		bookService.update(VALID_BOOK_ID,BOOK);
+
 		// then
+		Mockito.verify(bookRepository).saveAndFlush(BOOK);
 	}
 }
